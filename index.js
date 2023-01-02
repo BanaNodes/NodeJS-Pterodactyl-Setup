e50f5559feb593b24aa96a93c1556038b64d8510
BanaNodes <121801211+BanaNodes@users.noreply.github.com>
2023-01-02 14:08:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+require("dotenv").config();
+
 const TOKEN = process.env.TOKEN; // get TOKEN from .env file
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.MessageContent,
+    Discord.GatewayIntentBits.GuildMembers
+  ]
+});
+
+console.log("Logged in as "+client.user.tag)
+
+client.login(TOKEN);
